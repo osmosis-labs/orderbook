e6e5d58153dacf631b511866086bf876bc12566f
Connor Barr <crnbarr@gmail.com>
2024-03-12 12:04:27 +0000

    Small refactor

--- contracts/sumtree-orderbook/src/sumtree/test/test_node.rs
+++ contracts/sumtree-orderbook/src/sumtree/test/test_node.rs
@@ -45,40 +45,7 @@ fn test_node_insert_valid() {
     let tick_id = 1;
     let test_cases: Vec<TestNodeInsertCase> = vec![
         TestNodeInsertCase {
-            name: "Case: Left Empty Right Empty",
-            nodes: vec![NodeType::leaf(1u32, 10u32)],
-            expected: vec![1, 2],
-            print: true,
-        },
-        TestNodeInsertCase {
-            name: "Case: Left Leaf, Right Empty",
-            nodes: vec![NodeType::leaf(1u32, 10u32), NodeType::leaf(12u32, 10u32)],
-            expected: vec![1, 2, 3],
-            print: true,
-        },
-        TestNodeInsertCase {
-            name: "Case: Left Leaf, Right Leaf, Left Insert",
-            nodes: vec![
-                NodeType::leaf(1u32, 10u32),
-                NodeType::leaf(20u32, 10u32),
-                NodeType::leaf(12u32, 8u32),
-            ],
-            expected: vec![1, 5, 2, 4, 3],
-            print: true,
-        },
-        TestNodeInsertCase {
-            name: "Case: Left Internal, Right Leaf, Right Insert",
-            nodes: vec![
-                NodeType::leaf(1u32, 10u32),
-                NodeType::leaf(20u32, 10u32),
-                NodeType::leaf(12u32, 8u32),
-                NodeType::leaf(30u32, 8u32),
-            ],
-            expected: vec![1, 5, 2, 4, 7, 3, 6],
-            print: true,
-        },
-        TestNodeInsertCase {
-            name: "Case: Left Internal, Right Internal, Left Insert",
+            name: "Case 1a: Left Internal, Right Internal, Left Insert",
             nodes: vec![
                 NodeType::leaf(1u32, 5u32),
                 NodeType::leaf(20u32, 10u32),
@@ -90,7 +57,7 @@ fn test_node_insert_valid() {
             print: true,
         },
         TestNodeInsertCase {
-            name: "Case: Left Internal, Right Internal, Right Insert",
+            name: "Case 1b: Left Internal, Right Internal, Right Insert",
             nodes: vec![
                 NodeType::leaf(1u32, 11u32),
                 NodeType::leaf(20u32, 5u32),
@@ -101,10 +68,44 @@ fn test_node_insert_valid() {
             expected: vec![1, 5, 2, 4, 7, 9, 3, 8, 6],
             print: true,
         },
+        TestNodeInsertCase {
+            name: "Case 2: Left Empty Right Empty",
+            nodes: vec![NodeType::leaf(1u32, 10u32)],
+            expected: vec![1, 2],
+            print: true,
+        },
+        TestNodeInsertCase {
+            name: "Case 3: Left Leaf, Right Empty",
+            nodes: vec![NodeType::leaf(1u32, 10u32), NodeType::leaf(12u32, 10u32)],
+            expected: vec![1, 2, 3],
+            print: true,
+        },
+        TestNodeInsertCase {
+            name: "Case 4: Left Leaf, Right Leaf, Left Insert",
+            nodes: vec![
+                NodeType::leaf(1u32, 10u32),
+                NodeType::leaf(20u32, 10u32),
+                NodeType::leaf(12u32, 8u32),
+            ],
+            expected: vec![1, 5, 2, 4, 3],
+            print: true,
+        },
+        TestNodeInsertCase {
+            name: "Case 5: Left Internal, Right Leaf, Right Insert",
+            nodes: vec![
+                NodeType::leaf(1u32, 10u32),
+                NodeType::leaf(20u32, 10u32),
+                NodeType::leaf(12u32, 8u32),
+                NodeType::leaf(30u32, 8u32),
+            ],
+            expected: vec![1, 5, 2, 4, 7, 3, 6],
+            print: true,
+        },
     ];
 
     for test in test_cases {
         let mut deps = mock_dependencies();
+        // Create tree root
         let mut tree = TreeNode::new(
             book_id,
             tick_id,
@@ -112,6 +113,7 @@ fn test_node_insert_valid() {
             NodeType::internal(Uint128::zero(), (u32::MAX, u32::MIN)),
         );
 
+        // Insert nodes into tree
         for node in test.nodes {
             let mut tree_node = TreeNode::new(
                 book_id,
@@ -136,6 +138,7 @@ fn test_node_insert_valid() {
             println!();
         }
 
+        // Return tree in vector form from Depth First Search
         let result = tree.traverse(deps.as_ref().storage).unwrap();
 
         assert_eq!(
@@ -148,6 +151,7 @@ fn test_node_insert_valid() {
                 .collect::<Vec<TreeNode>>()
         );
 
+        // Ensure all internal nodes are correctly summed and contain correct ranges
         let internals: Vec<&TreeNode> = result.iter().filter(|x| x.is_internal()).collect();
         for internal_node in internals {
             let left_node = internal_node.get_left(deps.as_ref().storage).unwrap();
@@ -155,35 +159,26 @@ fn test_node_insert_valid() {
 
             let accumulated_value = left_node
                 .clone()
-                .map(|x| x.get_value())
-                .unwrap_or_default()
+                .map_or(Uint128::zero(), |x| x.get_value())
                 .checked_add(
                     right_node
                         .clone()
-                        .map(|x| x.get_value())
-                        .unwrap_or_default(),
+                        .map_or(Uint128::zero(), |x| x.get_value()),
                 )
                 .unwrap();
             assert_eq!(internal_node.get_value(), accumulated_value);
 
             let min = left_node
                 .clone()
-                .map(|n| n.get_min_range())
-                .unwrap_or(Uint128::MAX)
+                .map_or(Uint128::MAX, |n| n.get_min_range())
                 .min(
                     right_node
                         .clone()
-                        .map(|n| n.get_min_range())
-                        .unwrap_or(Uint128::MAX),
+                        .map_or(Uint128::MAX, |n| n.get_min_range()),
                 );
             let max = left_node
-                .map(|n| n.get_max_range())
-                .unwrap_or(Uint128::MIN)
-                .max(
-                    right_node
-                        .map(|n| n.get_max_range())
-                        .unwrap_or(Uint128::MIN),
-                );
+                .map_or(Uint128::MIN, |n| n.get_max_range())
+                .max(right_node.map_or(Uint128::MIN, |n| n.get_max_range()));
             assert_eq!(internal_node.get_min_range(), min);
             assert_eq!(internal_node.get_max_range(), max);
         }

f12edc9337553315c2d289f281284922750c107e
alpo <yukseloglua@berkeley.edu>
2024-03-22 16:55:21 -0700

    implement prefix sum algorithm

--- contracts/sumtree-orderbook/src/sumtree/tree.rs
+++ contracts/sumtree-orderbook/src/sumtree/tree.rs
@@ -1,6 +1,6 @@
 use super::node::{TreeNode, NODES};
 use crate::error::ContractResult;
-use cosmwasm_std::{Storage, Uint128};
+use cosmwasm_std::{ensure, Storage, Uint128};
 use cw_storage_plus::Map;
 
 pub const TREE: Map<&(u64, i64), u64> = Map::new("tree");
@@ -16,6 +16,7 @@ pub fn get_root_node(
     Ok(NODES.load(storage, &(book_id, tick_id, root_id))?)
 }
 
+#[allow(dead_code)]
 /// Calculates the prefix sum of values in the sumtree up to a target ETAS.
 pub fn get_prefix_sum(
     storage: &dyn Storage,
@@ -25,21 +26,104 @@ pub fn get_prefix_sum(
 ) -> ContractResult<Uint128> {
     let root_node = get_root_node(storage, book_id, tick_id)?;
 
-    // 1. Start from the root node and store its sum. This is the sum of the values of all the leaves.
-    let mut prefix_sum = TreeNode::get_value(&root_node);
+    // We start from the root node's sum, which includes everything in the tree.
+    // The prefix sum algorithm will chip away at this until we have the correct
+    // prefux sum in O(log(N)) time.
+    let starting_sum = TreeNode::get_value(&root_node);
 
-    // The logic below could be abstracted into a recursive helper that takes in a starting sum.
+    prefix_sum_walk(storage, &root_node, starting_sum, target_etas)
+}
 
-    // 2. Check the upper end of the left child's boundary. If this is <= the input ETAS, walk left. Otherwise, walk right.
-    let left_child = root_node.get_left(storage)?;
-    let right_child = root_node.get_right(storage)?;
+fn prefix_sum_walk(
+    storage: &dyn Storage,
+    node: &TreeNode,
+    mut current_sum: Uint128,
+    target_etas: Uint128,
+) -> ContractResult<Uint128> {
+    // Sanity check: target ETAS should be inside node's range.
+    if target_etas < node.get_min_range() {
+        // If the target ETAS is below the root node's range, we can return zero early.
+        return Ok(Uint128::zero());
+    } else if target_etas >= node.get_max_range() {
+        // If the target ETAS is above the root node's range, we can return the full sum early.
+        return Ok(current_sum);
+    }
 
-    // Determine which child to walk to next.
-    // Attempt to walk left. If it's none, check if right node exists. If not, exit early. If so, check its lower bound: if <= input ETAS, flag to walk right.
-    // If it exists, check its upper bound. If it's <= the input ETAS, flag to walk left. Otherwise, flag to walk right.
+    // If node is a leaf, we just return its full ETAS value. This is because by this point we
+    // know the target ETAS is in the node's range, and if the target ETAS is in the range of a
+    // leaf, we count the full leaf towards the prefix sum.
+    //
+    // Recall that the prefix sum is the sum of all the values of all leaves that have a _starting_
+    // ETAS below the target ETAS.
+    if !node.is_internal() {
+        return Ok(current_sum);
+    }
 
-    // 3. Whenever you are about to walk left, subtract the stored sum of your right child from your current sum. This is because at this stage, everything below the right child is at an ETAS > your input, so it should not be part of the prefix sum.
-    // 4. Repeat above until you either reach a node where the highest value is at an ETAS <= your input ETAS
+    // We fetch both children here since we need to access both regardless of
+    // whether we walk left or right.
+    let left_child = node.get_left(storage)?;
+    let right_child = node.get_right(storage)?;
 
-    Ok(prefix_sum)
+    // If the left child exists, we run the following logic:
+    // * If target ETAS < left child's lower bound, exit early with zero
+    // * Else if target ETAS <= upper bound, subtract right child sum from prefix sum and walk left
+    //
+    // If neither of the above conditions are met, we continue to logic around walking right.
+    if !left_child.is_none() {
+        let left_child = left_child.unwrap();
+        if target_etas < left_child.get_min_range() {
+            // If the target ETAS is below the left child's range, nothing in the
+            // entire tree should be included in the prefix sum, so we return zero.
+            return Ok(Uint128::zero());
+        }
+
+        if target_etas <= left_child.get_max_range() {
+            // Since the target ETAS is within the left child's range, we can safely conclude
+            // that everything below the right child should not be in our prefix sum.
+            let right_sum = if right_child.is_none() {
+                Uint128::zero()
+            } else {
+                TreeNode::get_value(&right_child.unwrap())
+            };
+
+            current_sum = current_sum.checked_sub(right_sum)?;
+
+            // Walk left recursively
+            current_sum = prefix_sum_walk(storage, &left_child, current_sum, target_etas)?;
+
+            return Ok(current_sum);
+        }
+    }
+
+    // --- Walk right ---
+
+    // If right child either doesn't exist, the current prefix sum is simply the sum of the left child,
+    // which is fully below the target ETAS, so we return the prefix sum as is.
+    if right_child.is_none() {
+        return Ok(current_sum);
+    }
+
+    // In the case where right child exists and the target ETAS is above the left child, we run the following logic:
+    // * If target ETAS < right child's lower bound: subtract right child's sum from prefix sum and return
+    // * If target ETAS <= right child's upper bound: walk right
+    // * If target ETAS > right child's upper bound: return full sum
+    let right_child = right_child.unwrap();
+    if target_etas < right_child.get_min_range() {
+        // If the ETAS is below the right child's range, we know that anything below the right child
+        // should not be included in the prefix sum. We subtract the right child's sum from the prefix sum.
+        current_sum = current_sum.checked_sub(TreeNode::get_value(&right_child))?;
+        return Ok(current_sum);
+    } else if target_etas <= right_child.get_max_range() {
+        // If the target ETAS falls in the right child's range, we need to walk right.
+        // We do not need to update the prefix sum here because we do not know how much
+        // to subtract from it yet. The right walk handles this update.
+
+        // Walk right recursively
+        current_sum = prefix_sum_walk(storage, &right_child, current_sum, target_etas)?;
+
+        return Ok(current_sum);
+    } else {
+        // If we reach here, everything in the tree is below the target ETAS, so we simply return the full sum.
+        return Ok(current_sum);
+    }
 }

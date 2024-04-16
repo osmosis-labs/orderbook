b63bf5167635f5204491366d4f5981eccb0eb703
Connor Barr <crnbarr@gmail.com>
2024-04-16 17:39:12 +0100

    test: Test scaffolding for swap_exact_amount_out

--- contracts/sumtree-orderbook/src/tests/test_sudo.rs
+++ contracts/sumtree-orderbook/src/tests/test_sudo.rs
@@ -6,7 +6,10 @@ use cosmwasm_std::{
 
 use crate::{
     orderbook::create_orderbook,
-    sudo::{dispatch_swap_exact_amount_in, ensure_fullfilment_amount, EXPECTED_SWAP_FEE},
+    sudo::{
+        dispatch_swap_exact_amount_in, dispatch_swap_exact_amount_out, ensure_fullfilment_amount,
+        EXPECTED_SWAP_FEE,
+    },
     types::{LimitOrder, OrderDirection, REPLY_ID_SUDO_SWAP_EX_AMT_IN},
     ContractError,
 };
@@ -309,6 +312,138 @@ fn test_swap_exact_amount_in() {
             test.token_in,
             test.token_out_denom.to_string(),
             test.token_out_min_amount,
+            test.swap_fee,
+        );
+
+        // -- Post test assertions --
+
+        // Assert expected error
+        if let Some(error) = test.expected_error {
+            assert_eq!(
+                error,
+                response.unwrap_err(),
+                "{}: did not receive expected error",
+                format_test_name(test.name)
+            );
+            continue;
+        }
+
+        // Response must be valid now
+        let response = response.unwrap();
+        assert_eq!(
+            response.messages.len(),
+            1,
+            "{}: invalid number of messages in response",
+            format_test_name(test.name)
+        );
+
+        // Ensure that generated fulfillment message matches what is expected
+        let bank_msg = response.messages.first().unwrap();
+        let expected_msg = SubMsg::reply_on_error(
+            BankMsg::Send {
+                to_address: sender.to_string(),
+                amount: vec![test.expected_output],
+            },
+            REPLY_ID_SUDO_SWAP_EX_AMT_IN,
+        );
+        assert_eq!(
+            bank_msg,
+            &expected_msg,
+            "{}: did not receive expected fulfillment message",
+            format_test_name(test.name)
+        );
+    }
+}
+
+struct SwapExactAmountOutTestCase {
+    name: &'static str,
+    pre_operations: Vec<OrderOperation>,
+    token_out: Coin,
+    token_in_denom: &'static str,
+    token_in_max_amount: Uint128,
+    swap_fee: Decimal,
+    expected_output: Coin,
+    expected_error: Option<ContractError>,
+}
+
+#[test]
+fn test_swap_exact_amount_out() {
+    let valid_book_id = 0;
+    let valid_tick_id = 0;
+    let quote_denom = "quote";
+    let base_denom = "base";
+    let sender = Addr::unchecked("sender");
+    let test_cases: Vec<SwapExactAmountOutTestCase> = vec![
+        SwapExactAmountOutTestCase {
+            name: "invalid in denom",
+            pre_operations: vec![],
+            token_out: coin(100u128, quote_denom),
+            token_in_denom: "notadenom",
+            token_in_max_amount: Uint128::from(100u128),
+            swap_fee: EXPECTED_SWAP_FEE,
+            expected_output: coin(100u128, quote_denom),
+            expected_error: Some(ContractError::InvalidPair {
+                token_in_denom: "notadenom".to_string(),
+                token_out_denom: quote_denom.to_string(),
+            }),
+        },
+        SwapExactAmountOutTestCase {
+            name: "invalid out denom",
+            pre_operations: vec![],
+            token_out: coin(100u128, "notadenom"),
+            token_in_denom: base_denom,
+            token_in_max_amount: Uint128::from(100u128),
+            swap_fee: EXPECTED_SWAP_FEE,
+            expected_output: coin(100u128, quote_denom),
+            expected_error: Some(ContractError::InvalidPair {
+                token_in_denom: base_denom.to_string(),
+                token_out_denom: "notadenom".to_string(),
+            }),
+        },
+        SwapExactAmountOutTestCase {
+            name: "invalid swap fee",
+            pre_operations: vec![],
+            token_out: coin(100u128, base_denom),
+            token_in_denom: "notadenom",
+            token_in_max_amount: Uint128::from(100u128),
+            swap_fee: Decimal::one(),
+            expected_output: coin(100u128, quote_denom),
+            expected_error: Some(ContractError::InvalidSwap {
+                error: format!(
+                    "Provided swap fee does not match: expected {EXPECTED_SWAP_FEE} received {}",
+                    Decimal::one()
+                ),
+            }),
+        },
+    ];
+
+    for test in test_cases {
+        // -- Test Setup --
+        let mut deps = mock_dependencies();
+        let env = mock_env();
+        let info = mock_info(sender.as_str(), &[]);
+        create_orderbook(
+            deps.as_mut(),
+            env.clone(),
+            info.clone(),
+            quote_denom.to_string(),
+            base_denom.to_string(),
+        )
+        .unwrap();
+
+        // Run any pre-operations
+        for op in test.pre_operations {
+            op.run(deps.as_mut(), env.clone(), info.clone(), valid_book_id)
+                .unwrap();
+        }
+
+        // -- System under test --
+        let response = dispatch_swap_exact_amount_out(
+            deps.as_mut(),
+            sender.to_string(),
+            test.token_in_denom.to_string(),
+            test.token_in_max_amount,
+            test.token_out,
             test.swap_fee,
         );
 

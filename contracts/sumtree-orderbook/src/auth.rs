ab4ed08d1080470da753405713f5759229cb3e31
Connor Barr <crnbarr@gmail.com>
2024-04-21 18:44:38 +0100

    feat: added admin auth handlers

---
+++ contracts/sumtree-orderbook/src/auth.rs
@@ -0,0 +1,37 @@
+use crate::{error::ContractResult, ContractError};
+use cosmwasm_std::{ensure, Addr, Deps, DepsMut, MessageInfo, Response};
+use cw_storage_plus::Item;
+
+pub const ADMIN: Item<Addr> = Item::new("admin");
+pub const ADMIN_OFFER: Item<Addr> = Item::new("admin_offer");
+
+pub(crate) fn dispatch_transfer_admin(
+    deps: DepsMut,
+    info: MessageInfo,
+    new_admin: Addr,
+) -> ContractResult<Response> {
+    ensure_is_admin(deps.as_ref(), &info.sender)?;
+
+    transfer_admin(deps, new_admin.clone())?;
+
+    Ok(Response::default().add_attributes(vec![
+        ("method", "transfer_admin"),
+        ("new_admin", new_admin.as_str()),
+    ]))
+}
+
+pub(crate) fn transfer_admin(deps: DepsMut, new_admin: Addr) -> ContractResult<()> {
+    deps.api.addr_validate(new_admin.as_str())?;
+    ADMIN_OFFER.save(deps.storage, &new_admin)?;
+    Ok(())
+}
+
+pub(crate) fn ensure_is_admin(deps: Deps, sender: &Addr) -> ContractResult<()> {
+    let admin = ADMIN
+        .load(deps.storage)
+        .ok()
+        .ok_or(ContractError::Unauthorized {})?;
+    ensure!(admin == sender, ContractError::Unauthorized {});
+
+    Ok(())
+}
--- contracts/sumtree-orderbook/src/lib.rs
+++ contracts/sumtree-orderbook/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod auth;
 pub mod constants;
 pub mod contract;
 mod error;

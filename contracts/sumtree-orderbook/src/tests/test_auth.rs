a375c2634842b9fa1ced31be2e3aba4a54f5380b
Connor Barr <crnbarr@gmail.com>
2024-04-21 19:03:21 +0100

    test: added cancel transfer admin tests

--- contracts/sumtree-orderbook/src/tests/test_auth.rs
+++ contracts/sumtree-orderbook/src/tests/test_auth.rs
@@ -4,7 +4,7 @@ use cosmwasm_std::{
 };
 
 use crate::{
-    auth::{dispatch_transfer_admin, ADMIN, ADMIN_OFFER},
+    auth::{dispatch_cancel_transfer_admin, dispatch_transfer_admin, ADMIN, ADMIN_OFFER},
     ContractError,
 };
 
@@ -68,6 +68,75 @@ fn test_transfer_admin() {
         let new_admin = ADMIN_OFFER.load(deps.as_ref().storage).unwrap();
         assert_eq!(
             new_admin, test.new_admin,
+            "{}: admin offer was not correctly set",
+            test.name
+        );
+    }
+}
+
+struct CancelTransferAdminTestCase {
+    name: &'static str,
+    current_admin: &'static str,
+    new_admin: &'static str,
+    expected_error: Option<ContractError>,
+}
+
+#[test]
+fn test_cancel_transfer_admin() {
+    let sender = "sender";
+    let new_admin = "new_admin";
+    let test_cases = vec![
+        CancelTransferAdminTestCase {
+            name: "valid transfer",
+            current_admin: sender,
+            new_admin,
+            expected_error: None,
+        },
+        CancelTransferAdminTestCase {
+            name: "unauthorized",
+            current_admin: "notthesender",
+            new_admin,
+            expected_error: Some(ContractError::Unauthorized {}),
+        },
+    ];
+
+    for test in test_cases {
+        // -- Test Setup --
+        let mut deps = mock_dependencies();
+        let info = mock_info(sender, &[]);
+
+        // Store current admin
+        ADMIN
+            .save(deps.as_mut().storage, &Addr::unchecked(test.current_admin))
+            .unwrap();
+        // Store admin transfer
+        ADMIN_OFFER
+            .save(deps.as_mut().storage, &Addr::unchecked(test.new_admin))
+            .unwrap();
+
+        // -- System under test --
+        let res = dispatch_cancel_transfer_admin(deps.as_mut(), info);
+
+        // Assert expected error
+        if let Some(err) = test.expected_error {
+            assert_eq!(
+                res.unwrap_err(),
+                err,
+                "{}: did not receive expected error",
+                test.name
+            );
+
+            // Ensure nothing is stored in state
+            assert!(ADMIN_OFFER.load(deps.as_ref().storage).unwrap() == test.new_admin);
+            continue;
+        }
+
+        // Assert the offer has been rescinded
+        assert!(
+            ADMIN_OFFER
+                .may_load(deps.as_ref().storage)
+                .unwrap()
+                .is_none(),
             "{}: admin offer was not correctly set",
             test.name
         );

bbff0e9949c24a305c60751cd03c0fd14dc7daf5
Connor Barr <crnbarr@gmail.com>
2024-04-21 19:54:13 +0100

    chore: removed template comments

--- contracts/sumtree-orderbook/src/contract.rs
+++ contracts/sumtree-orderbook/src/contract.rs
@@ -30,8 +30,6 @@ pub fn instantiate(
 
     create_orderbook(deps, msg.quote_denom.clone(), msg.base_denom.clone())?;
 
-    // With `Response` type, it is possible to dispatch message to invoke external logic.
-    // See: https://github.com/CosmWasm/cosmwasm/blob/main/SEMANTICS.md#dispatching-messages
     Ok(Response::new().add_attributes(vec![
         ("method", "instantiate"),
         ("quote_denom", &msg.quote_denom),
@@ -39,22 +37,11 @@ pub fn instantiate(
     ]))
 }
 
-/// Handling contract migration
-/// To make a contract migratable, you need
-/// - this entry_point implemented
-/// - only contract admin can migrate, so admin has to be set at contract initiation time
-/// Handling contract execution
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn migrate(_deps: DepsMut, _env: Env, msg: MigrateMsg) -> Result<Response, ContractError> {
-    match msg {
-        // Find matched incoming message variant and execute them with your custom logic.
-        //
-        // With `Response` type, it is possible to dispatch message to invoke external logic.
-        // See: https://github.com/CosmWasm/cosmwasm/blob/main/SEMANTICS.md#dispatching-messages
-    }
+    match msg {}
 }
 
-/// Handling contract execution
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
     deps: DepsMut,
@@ -151,12 +138,8 @@ pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> ContractResult<Binary> {
     }
 }
 
-/// Handling submessage reply.
-/// For more info on submessage and reply, see https://github.com/CosmWasm/cosmwasm/blob/main/SEMANTICS.md#submessages
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn reply(_deps: DepsMut, _env: Env, msg: Reply) -> Result<Response, ContractError> {
-    // With `Response` type, it is still possible to dispatch message to invoke external logic.
-    // See: https://github.com/CosmWasm/cosmwasm/blob/main/SEMANTICS.md#dispatching-messages
     ensure!(
         msg.result.is_ok(),
         ContractError::ReplyError {

e426bb5ea9334687ab0155b7c02526218cd62372
Connor Barr <crnbarr@gmail.com>
2024-04-21 19:20:43 +0100

    test: added test for reject admin transfer

--- contracts/sumtree-orderbook/src/tests/test_auth.rs
+++ contracts/sumtree-orderbook/src/tests/test_auth.rs
@@ -5,8 +5,8 @@ use cosmwasm_std::{
 
 use crate::{
     auth::{
-        dispatch_cancel_admin_transfer, dispatch_claim_admin, dispatch_transfer_admin, ADMIN,
-        ADMIN_OFFER,
+        dispatch_cancel_admin_transfer, dispatch_claim_admin, dispatch_reject_admin_transfer,
+        dispatch_transfer_admin, ADMIN, ADMIN_OFFER,
     },
     ContractError,
 };
@@ -214,6 +214,102 @@ fn test_claim_admin() {
         assert_eq!(
             ADMIN.may_load(deps.as_ref().storage).unwrap(),
             test.new_admin.map(Addr::unchecked),
+            "{}: admin was not correctly claimed",
+            test.name
+        );
+        // Assert the offer has been removed
+        assert!(
+            ADMIN_OFFER
+                .may_load(deps.as_ref().storage)
+                .unwrap()
+                .is_none(),
+            "{}: admin offer not correctly removed",
+            test.name
+        );
+    }
+}
+
+struct RejectAdminTransferTestCase {
+    name: &'static str,
+    sender: &'static str,
+    new_admin: Option<&'static str>,
+    expected_error: Option<ContractError>,
+}
+
+#[test]
+fn test_reject_admin_transfer() {
+    let new_admin = "new_admin";
+    let current_admin = "current_admin";
+    let test_cases = vec![
+        RejectAdminTransferTestCase {
+            name: "valid rejection",
+            sender: new_admin,
+            new_admin: Some(new_admin),
+            expected_error: None,
+        },
+        RejectAdminTransferTestCase {
+            name: "no offer",
+            sender: new_admin,
+            new_admin: None,
+            expected_error: Some(ContractError::Unauthorized {}),
+        },
+        RejectAdminTransferTestCase {
+            name: "unauthorized",
+            sender: "notthenewadmin",
+            new_admin: Some(new_admin),
+            expected_error: Some(ContractError::Unauthorized {}),
+        },
+    ];
+
+    for test in test_cases {
+        // -- Test Setup --
+        let mut deps = mock_dependencies();
+        let info = mock_info(test.sender, &[]);
+
+        // Store current admin for post check
+        ADMIN
+            .save(deps.as_mut().storage, &Addr::unchecked(current_admin))
+            .unwrap();
+
+        // Save admin offer if one is required
+        if let Some(new_admin) = test.new_admin {
+            ADMIN_OFFER
+                .save(deps.as_mut().storage, &Addr::unchecked(new_admin))
+                .unwrap();
+        }
+
+        // -- System under test --
+        let res = dispatch_reject_admin_transfer(deps.as_mut(), info);
+
+        // Assert expected error
+        if let Some(err) = test.expected_error {
+            assert_eq!(
+                res.unwrap_err(),
+                err,
+                "{}: did not receive expected error",
+                test.name
+            );
+
+            // Ensure state remains unchanged
+            assert_eq!(
+                ADMIN.load(deps.as_ref().storage).unwrap(),
+                Addr::unchecked(current_admin),
+                "{}: invalid admin stored",
+                test.name
+            );
+            assert_eq!(
+                ADMIN_OFFER.may_load(deps.as_ref().storage).unwrap(),
+                test.new_admin.map(Addr::unchecked),
+                "{}: admin offer was unexpectedly altered",
+                test.name
+            );
+            continue;
+        }
+
+        // Assert the admin has been claimed
+        assert_eq!(
+            ADMIN.load(deps.as_ref().storage).unwrap(),
+            Addr::unchecked(current_admin),
             "{}: admin was not correctly claimed",
             test.name
         );

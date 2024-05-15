61a65bbe69ac60ff7335d90c152d4b2fb6628674
Connor Barr <crnbarr@gmail.com>
2024-05-15 16:22:39 +0100

    chore: added comments to mock_querier

--- contracts/sumtree-orderbook/src/tests/mock_querier.rs
+++ contracts/sumtree-orderbook/src/tests/mock_querier.rs
@@ -7,10 +7,7 @@ use cosmwasm_std::{
 
 use super::test_constants::{BASE_DENOM, QUOTE_DENOM};
 
-pub(crate) struct WasmMockQuerier {
-    pub base: MockQuerier,
-}
-
+/// Creates mock dependencies with a custom querier (`WasmMockQuerier`)
 pub(crate) fn mock_dependencies_custom() -> OwnedDeps<MockStorage, MockApi, WasmMockQuerier> {
     let custom_querier: WasmMockQuerier = WasmMockQuerier::new(MockQuerier::new(&[]));
     let storage = MockStorage::default();
@@ -22,6 +19,11 @@ pub(crate) fn mock_dependencies_custom() -> OwnedDeps<MockStorage, MockApi, Wasm
     }
 }
 
+pub(crate) struct WasmMockQuerier {
+    pub base: MockQuerier,
+}
+
+/// Custom querier must implement `Querier` to be usable in entry points
 impl Querier for WasmMockQuerier {
     fn raw_query(&self, bin_request: &[u8]) -> QuerierResult {
         let request: QueryRequest<cosmwasm_std::Empty> = match from_json(bin_request) {
@@ -38,18 +40,22 @@ impl Querier for WasmMockQuerier {
 }
 
 impl WasmMockQuerier {
+    /// Handles a query request
     pub(crate) fn handle_query(
         &self,
         querier: &MockQuerier,
         request: &QueryRequest<cosmwasm_std::Empty>,
     ) -> QuerierResult {
         match &request {
+            // The default querier does not have a correct handler for `BankQuery::Supply`
             QueryRequest::Bank(BankQuery::Supply { denom }) => match denom.as_str() {
+                // `BASE_DENOM` is a valid denom for test cases
                 BASE_DENOM => {
                     let mut resp = SupplyResponse::default();
                     resp.amount = coin(1000000000000u128, denom);
                     QuerierResult::Ok(ContractResult::Ok(to_json_binary(&resp).unwrap()))
                 }
+                // `QUOTE_DENOM` is a valid denom for test cases
                 QUOTE_DENOM => {
                     let mut resp = SupplyResponse::default();
                     resp.amount = coin(1000000000000u128, denom);
@@ -57,6 +63,7 @@ impl WasmMockQuerier {
                 }
                 _ => querier.handle_query(request),
             },
+            // Every other query can be handled as usual
             _ => querier.handle_query(request),
         }
     }

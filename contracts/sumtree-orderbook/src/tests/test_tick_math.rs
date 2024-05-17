79de4f1fdae59d1bf8307ffba63a4653adf36d93
Connor Barr <crnbarr@gmail.com>
2024-05-17 22:04:35 +0100

    [Sumtree]: Max Spot Price (#159)
    * feat: added max spot price check [WIP]
    
    * fix: fixed max spot check
    
    * test: fixed failing tests
    
    * fix: updated max tick and max spot price, fixed tests accordingly
    
    * refactor: removed unnecessary check
    
    * fix: spot price for claimed_price when placing an order is now based on order direction
    
    * remove unnecessary & expensive check
    
    ---------
    
    Co-authored-by: alpo <yukseloglua@berkeley.edu>

--- contracts/sumtree-orderbook/src/tests/test_tick_math.rs
+++ contracts/sumtree-orderbook/src/tests/test_tick_math.rs
@@ -16,10 +16,10 @@ struct TickToPriceTestCase {
 fn test_tick_to_price() {
     // This constant is used to test price iterations near max tick.
     // It essentially derives the amount we expect price to increment by,
-    // which with an EXPONENT_AT_PRICE_ONE of -6 should be 10^31.
+    // which with an EXPONENT_AT_PRICE_ONE of -6 should be 10^14.
     let min_increment_near_max_price = Decimal256::from_ratio(
         Uint256::from(10u8)
-            .checked_pow((37 + EXPONENT_AT_PRICE_ONE) as u32)
+            .checked_pow((20 + EXPONENT_AT_PRICE_ONE) as u32)
             .unwrap(),
         Uint256::one(),
     );
@@ -129,7 +129,11 @@ fn test_tick_to_price() {
 
         match test.expected_error {
             Some(expected_err) => assert_eq!(result.unwrap_err(), expected_err),
-            None => assert_eq!(test.expected_price, result.unwrap()),
+            None => assert_eq!(
+                test.expected_price,
+                result.unwrap(),
+                "expected price and result did not match"
+            ),
         }
     }
 }

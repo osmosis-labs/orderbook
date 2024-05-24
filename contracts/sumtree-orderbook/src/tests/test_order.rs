cf2f6373993fecbdcfb1cbf7172f3d8fec735bfe
Connor Barr <crnbarr@gmail.com>
2024-05-24 20:53:28 +0100

    [Sumtree]: Claim and Cancel Response Attributes (#177)
    * feat: added events for claiming an order
    
    * feat: added placed quantity to claim event
    
    * test: fixed failing tests

--- contracts/sumtree-orderbook/src/tests/test_order.rs
+++ contracts/sumtree-orderbook/src/tests/test_order.rs
@@ -514,6 +514,7 @@ fn test_cancel_limit() {
             "{}",
             format_test_name(test.name)
         );
+        
         assert_eq!(
             response.attributes[1],
             ("owner", test.owner),
@@ -529,6 +530,18 @@ fn test_cancel_limit() {
         assert_eq!(
             response.attributes[3],
             ("order_id", test.order_id.to_string()),
+            "{}",
+            format_test_name(test.name)
+        );
+        assert_eq!(
+            response.attributes[4],
+            ("quantity", test.quantity.to_string()),
+            "{}",
+            format_test_name(test.name)
+        );
+        assert_eq!(
+            response.attributes[5],
+            ("order_direction", test.order_direction.to_string()),
             "{}",
             format_test_name(test.name)
         );
@@ -3975,7 +3988,7 @@ fn test_maker_fee() {
             continue;
         } 
 
-        let (_, msgs) = result.unwrap();
+        let (_, msgs, _) = result.unwrap();
 
         // The claimer's message is always first in the array of bank messages
         let claimer_msg = msgs.first().unwrap();
